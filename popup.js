9fbac9436fa098f348c5fcaf30ebc217f4452d26
oalberto96 <ochoapublic@gmail.com>
2021-03-21 12:08:02 -0700

    add boundaries to not mark every word

--- popup.js
+++ popup.js
@@ -11,29 +11,37 @@ changeColor.addEventListener('click', async () => {
 // The body of this function will be executed as a content script inside the
 // current page
 function setPageBackgroundColor() {
-  let iframe = document.getElementById('KindleReaderIFrame');
-  let innerDoc = iframe.contentDocument || iframe.contentWindow.document;
-  const touchLayer = innerDoc.getElementById('kindleReader_touchLayer');
-  touchLayer.remove();
-  iframe = innerDoc.getElementById('column_0_frame_1');
+  let readerIframe = document.getElementById('KindleReaderIFrame');
+  let iframe = null;
+  let innerDoc =
+    readerIframe.contentDocument || readerIframe.contentWindow.document;
+  const iframes = innerDoc.querySelectorAll('iframe');
+  for (let i = 0; i < iframes.length; i++) {
+    if (iframes[i].style.marginTop === '0px') {
+      iframe = iframes[i];
+      break;
+    }
+  }
   innerDoc = iframe.contentDocument || iframe.contentWindow.document;
-  const pageBreak = innerDoc.querySelector('.page-break');
-  const contentWrapper = pageBreak.parentElement;
-  let startSearch = false;
+  const divs = innerDoc.querySelectorAll('body > div');
+  const contentWrapper = divs[2];
+  let startSearch = true;
   const words = [];
   contentWrapper.childNodes.forEach(node => {
-    if (node === pageBreak) {
-      startSearch = true;
-      return;
-    }
     if (startSearch) {
       let queue = [node];
       let current = null;
       while (queue.length > 0) {
         current = queue.shift();
         if (current.tagName) {
-          if (current.tagName === 'SPAN') {
-            words.push(current);
+          if (
+            current.tagName === 'SPAN' &&
+            current.className !== 'page-break'
+          ) {
+            const { top } = current.getBoundingClientRect();
+            if (top >= 0 && top < window.innerHeight) {
+              words.push(current);
+            }
           } else {
             current.childNodes.forEach(child => queue.push(child));
           }
@@ -42,13 +50,18 @@ function setPageBackgroundColor() {
     }
   });
   const readText = async () => {
-    for (let i = 0; i < words.length; i++) {
+    console.log(words[0]);
+    const originalStyle = words[0].style;
+    for (let i = 1; i < words.length; i++) {
       currentWord = await new Promise(resolve => {
         setTimeout(() => {
-          words[i].style.color = 'red';
-          console.log(words[i]);
+          words[i - 1].style = originalStyle;
+          words[i].style.color = '#fbf0d9';
+          words[i].style.backgroundColor = '#9e9174';
+          const { top, bottom } = words[i].getBoundingClientRect();
+          console.log(words[i], top, bottom);
           resolve();
-        }, 200);
+        }, 150);
       });
     }
   };
