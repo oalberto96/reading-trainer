8c7afb9c5e9c84778f9a295a03fc8da4cca02bb1
oalberto96 <ochoapublic@gmail.com>
2021-03-20 22:21:27 -0700

    mark words

--- popup.js
+++ popup.js
@@ -11,7 +11,47 @@ changeColor.addEventListener('click', async () => {
 // The body of this function will be executed as a content script inside the
 // current page
 function setPageBackgroundColor() {
-  chrome.storage.sync.get('color', ({ color }) => {
-    document.body.style.backgroundColor = color;
+  let iframe = document.getElementById('KindleReaderIFrame');
+  let innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+  const touchLayer = innerDoc.getElementById('kindleReader_touchLayer');
+  touchLayer.remove();
+  iframe = innerDoc.getElementById('column_0_frame_1');
+  innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+  const pageBreak = innerDoc.querySelector('.page-break');
+  const contentWrapper = pageBreak.parentElement;
+  let startSearch = false;
+  const words = [];
+  contentWrapper.childNodes.forEach(node => {
+    if (node === pageBreak) {
+      startSearch = true;
+      return;
+    }
+    if (startSearch) {
+      let queue = [node];
+      let current = null;
+      while (queue.length > 0) {
+        current = queue.shift();
+        if (current.tagName) {
+          if (current.tagName === 'SPAN') {
+            words.push(current);
+          } else {
+            current.childNodes.forEach(child => queue.push(child));
+          }
+        }
+      }
+    }
   });
+  const readText = async () => {
+    for (let i = 0; i < words.length; i++) {
+      currentWord = await new Promise(resolve => {
+        setTimeout(() => {
+          words[i].style.color = 'red';
+          console.log(words[i]);
+          resolve();
+        }, 200);
+      });
+    }
+  };
+  readText();
+  console.log(words);
 }
